37076b117d4cfccf16c9a0e3573cc30a1c69cf60
kaczanowski.mateusz@gmail.com <kaczanowski.mateusz@gmail.com>
2015-03-07 12:30:07 +0000

    fleet

--- archlinux-fleet/run.sh
+++ archlinux-fleet/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-CONTAINER=$(docker run --privileged -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro mkaczanowski/archlinux-fleet)
+CONTAINER=$(docker run -d mkaczanowski/archlinux-fleet)
 echo "to enter docker container run: docker exec -t -i ${CONTAINER} /bin/bash"
