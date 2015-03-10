d843cfb51c9b2c0ad31871d75b05ed1282d2a31f
root <root@odroid3.lan>
2015-03-10 14:47:23 -0400

    nginx volume

--- archlinux-nginx/run.sh
+++ archlinux-nginx/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-CONTAINER=$(sudo docker run --privileged -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 80:80 -p 443:443 mkaczanowski/archlinux-nginx)
+CONTAINER=$(sudo docker run --privileged -d -v /srv/http:/srv/http -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 80:80 -p 443:443 mkaczanowski/archlinux-nginx)
 echo "to enter docker container run: docker exec -t -i ${CONTAINER} /bin/bash"
